f254d096f35f67f6b2b009d958188ccafeb860b4
Zoim <42458629+moizl599@users.noreply.github.com>
2023-09-26 13:40:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,74 @@
-from SigSciApiPy.SigSci import *
-import os.path
 import json
-from random import random
-# setup sigsci api module
-sigsci = SigSciAPI()
+import os
+import glob
+from SigSciApiPy.SigSci import SigSciAPI
 
-# required variables
-sigsci.email = "moiz.lakdawala@workjam.com"
-sigsci.corp = "workjam"
-sigsci.site = "gcp-prod"
-sigsci.api_token = "fa003294-5c0d-48ac-be69-67fe01e2b55a"
-tags = ['SQLI']
-sigsci.from_time = "-30min"
-if sigsci.authenticate():
-    for tag in tags:
-        signal_science_logs = {'WAF_timestamp':'',
-                               'WAF_source':'',
-                               'WAF_Remote_country':'',
-                               'WAF_action':'',
-                               'WAF_reason':'',
-                               }
-        sigsci.file = f"C:\\Users\\Moiz\\logs\\{tag}.json"
-        logfile= f"C:\\Users\\Moiz\\logs\\{tag}_logs.log"
-        if os.path.isfile(sigsci.file):
-            os.remove(sigsci.file)
-        if os.path.isfile(logfile):
-            os.remove(logfile)
-        with open(logfile, 'x') as logs_file:
-            test = sigsci.get_list_events(tag)
-            data = test['data']
-            for z in range(len(data)):
-                    extracted_data = data[z]
-                    res = {key: extracted_data[key] for key in extracted_data.keys()
-        & {'timestamp','source','remoteCountryCode','action','reasons'}}
-                    signal_science_logs = {'WAF_timestamp':res['timestamp'],
-                               'WAF_source':res['source'],
-                               'WAF_Remote_country':res['remoteCountryCode'],
-                               'WAF_action':res['action'],
-                               'WAF_reason':res['reasons'],
-                               }
-                    with open(logfile, 'a') as f:
-                         json.dump(signal_science_logs,f)
-                         f.write('\n')
 
-             
-       
+class SignalScience:
+    def __init__(self, config_file='key.cfg'):
+        """
+        Initializes SignalScience object with required configurations and parameters from the config file.
+
+        :param config_file: The path to the configuration file. Default is 'key.cfg'.
+        """
+        with open(config_file) as f:
+            config = json.load(f)
+
+        self.sigsci = SigSciAPI()
+        self.sigsci.email = config['email']
+        self.sigsci.corp = config['corp']
+        self.sigsci.site = config['site']
+        self.sigsci.api_token = config['api_token']
+        self.tags = config['tags']
+        self.sigsci.from_time = config.get('from_time', '-5min')  # Defaults to '-5min' if not provided in config file
+
+    def run(self):
+        """
+        Executes the API call to fetch events, logs events per tag and then merges all log files into a master log file.
+        """
+        if self.sigsci.authenticate():
+            for tag in self.tags:
+                logfile = f"/opt/wazuh_logging/signal_science/logfiles/{tag}_logs.log"
+
+                # Remove existing log file
+                if os.path.isfile(logfile):
+                    os.remove(logfile)
+
+                # Open the log file and write events fetched from API
+                with open(logfile, 'a') as logs_file:
+                    events = self.sigsci.get_list_events(tag)['data']
+                    for event in events:
+                        res = {key: event[key] for key in {'timestamp', 'source', 'remoteCountryCode', 'action', 'reasons'}}
+                        log_entry = {
+                            'WAF_timestamp': res.get('timestamp', ''),
+                            'WAF_source': res.get('source', ''),
+                            'WAF_Remote_country': res.get('remoteCountryCode', ''),
+                            'WAF_action': res.get('action', ''),
+                            'WAF_reason': res.get('reasons', ''),
+                        }
+                        json.dump(log_entry, logs_file)
+                        logs_file.write('\n')
+
+            # Merge all individual log files into a master log file
+            master_logfile = '/opt/wazuh_logging/signal_science/masterlogfile/master_logfile.log'
+            if os.path.isfile(master_logfile):
+                os.remove(master_logfile)
+
+            files = glob.glob('/opt/wazuh_logging/signal_science/logfiles/*.log')
+            with open(master_logfile, 'w') as result:
+                for file in files:
+                    with open(file, 'r') as f:
+                        for line in f:
+                            result.write(line)
+
+
+def main():
+    """
+    The main function to instantiate the SignalScience object and run the event fetching and logging.
+    """
+    signal_science = SignalScience()
+    signal_science.run()
+
+
+if __name__ == "__main__":
+    main()
